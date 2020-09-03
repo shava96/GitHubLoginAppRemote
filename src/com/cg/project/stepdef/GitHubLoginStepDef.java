8d68c514bb442df03fe46234509b4266e8f57121
Shashwat Vats <shashwat.singh.vats@gmail.com>
2020-09-03 10:16:43 +0530

    new updates

--- src/com/cg/project/stepdef/GitHubLoginStepDef.java
+++ src/com/cg/project/stepdef/GitHubLoginStepDef.java
@@ -18,7 +18,7 @@ public class GitHubLoginStepDef {
 	private GitHubLoginPage loginPage;	
 	@Before
 	public void setupStepEnv() {
-		System.setProperty("webdriver.chrome.driver", "C:\\SatishTrainingData\\TrainingsTopicsWise\\SDET\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Software\\CI software\\chromedriver_win32\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.MILLISECONDS);	
